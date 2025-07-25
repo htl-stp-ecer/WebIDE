5248e1d13501dc8c366d21400bc5408d37e9c5b7
Kuchen <daniel.schneeweis@a1.net>
2025-07-25 21:38:24 +0200

    finish skeleton

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {InputGroup} from 'primeng/inputgroup';
+import {InputText} from 'primeng/inputtext';
+import {Button} from 'primeng/button';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    InputGroup,
+    InputText,
+    Button
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
